282146f4e69e5f5fdde1d5a3dad3d898d86a2ee2
fbbzb <151777732+fbbzb@users.noreply.github.com>
2024-06-28 03:49:28 +0300

    Add files via upload

---
+++ лр 9/Задание 2 (2).js
@@ -0,0 +1,12 @@
+let fruit = 'apple';
+
+switch (fruit) {
+    case 'apple':
+        console.log('Apple is selected');
+        break;
+    case 'banana':
+        console.log('Banana is selected');
+        break;
+    default:
+        console.log('Unknown fruit');
+}
---
+++ лр 9/Задание 2 (5).js
@@ -0,0 +1,6 @@
+let counter = 0;
+
+do {
+    console.log('Counter is: ' + counter);
+    counter++;
+} while (counter < 5);
